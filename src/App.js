9a5c7af320365928a567806c9438cec044d2070e
madgrv <mad.grv@gmail.com>
2023-07-03 04:41:32 +0100

    Updated form submit and setup data table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
+import DataTable from './components/DataTable';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <br />
+      {/* <DataTable /> */}
     </div>
   );
 }
