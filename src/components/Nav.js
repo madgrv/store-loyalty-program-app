c042a52c1ff31b397dce451e05f460cc0fb50fb3
madgrv <mad.grv@gmail.com>
2023-07-03 10:40:10 +0100

    Added login placeholder with conditional rendering

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,24 @@
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/nav.module.css'
 
 export default function Nav() {
+    const [ isLoggedIn, setIsLoggedIn ] = React.useState(false)
+
+    const handleLogin = () => {
+        setIsLoggedIn((prevState) => prevState = !prevState)
+    }
     
     return(
         <nav className={styles.nav}>
             <ul className={styles.navMenu}>
                 <li>Home</li>
                 <li>Back</li>
-                <li className={styles.logoutButton}>Logout</li>
+                { isLoggedIn ? 
+                    <li className={styles.loginButton} onClick={handleLogin}>Logout</li>
+                    :
+                    <li className={styles.loginButton} onClick={handleLogin}>Login</li>
+                }
+                
             </ul>
         </nav>
     )
