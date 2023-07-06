94268e648f17afc5c2eb15c2149d5ad4a25e50e2
madgrv <mad.grv@gmail.com>
2023-07-06 10:53:13 +0100

    Added login form and adjusted style for accessibility

---
+++ src/components/LoginForm.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import Styles from '../styles/loginForm.module.css';
+
+
+export default function LoginFormI({handleLogin}) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const userInfo = {
+            username,
+            password
+          };
+
+        handleLogin(userInfo);    
+    }
+
+    return(
+        <div className={Styles.loginFormContainer}>
+            <form className={Styles.loginForm}>
+                <fieldset className={Styles.loginFieldset}>
+                    <div className={Styles.inputField}>
+                        {/* <label>User:</label> */}
+                        <input 
+                            className={Styles.input} 
+                            type='text' 
+                            placeholder='User name'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className={Styles.inputField}>
+                        {/* <label>Password:</label> */}
+                        <input 
+                            className={Styles.input} 
+                            type='password' 
+                            placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button className={Styles.button} onClick={handleClick}>Login</button>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,55 @@
 import React, { useState, useEffect } from 'react'
 import styles from '../styles/nav.module.css'
+import LoginForm from './LoginForm'
 
 export default function Nav() {
-    const [ isLoggedIn, setIsLoggedIn ] = React.useState(false)
+    const [ isLoggedIn, setIsLoggedIn ] = React.useState(false);
+    const [ isOpen, setIsOpen ] = React.useState(false);
+    const [userName, setUserName] = useState('');
 
-    const handleLogin = () => {
-        setIsLoggedIn((prevState) => prevState = !prevState)
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    const handleLogin = (userInfo) => {
+        // setIsLoggedIn((prevState) => prevState = !prevState);
+        const response = { userName: userInfo.username };
+        setUserName(response.userName);
+        setIsLoggedIn(true);
+        setIsOpen(!isOpen);
+        console.log(userInfo)
     }
+
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+      };
+
+
     
     return(
-        <nav className={styles.nav}>
-            <ul className={styles.navMenu}>
-                <li>Home</li>
-                <li>Back</li>
-                { isLoggedIn ? 
-                    <li className={styles.loginButton} onClick={handleLogin}>Logout</li>
-                    :
-                    <li className={styles.loginButton} onClick={handleLogin}>Login</li>
-                }
-                
-            </ul>
-        </nav>
+        <div className={styles.navContainer}>
+            <nav className={styles.nav}>
+                <ul className={styles.navMenu}>
+                    <li>Home</li>
+                    <li>Back</li>
+                    { isLoggedIn ? 
+                        <li className={styles.loginButton} onClick={handleLogout}>Logout</li>
+                        :
+                        <li className={styles.loginButton} onClick={toggle}>Login</li>
+                    }
+                </ul>
+            </nav>
+            {isLoggedIn ? <p className={styles.userWelcome}>Welcome, {userName}</p>
+                :
+                <p className={styles.userWelcome}>Please login</p>
+            }
+            {
+                isOpen && (
+                    <div className={styles.loginFormWrapper}>
+                        <LoginForm handleLogin={handleLogin}/>
+                    </div>
+                )
+            }
+    
+        </div>
     )
 }
